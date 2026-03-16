9f96070846e9417c43079a92079efd15c367d086
Wyatt Thale <wyattthale@outlook.com>
2026-03-16 16:04:37 -0700

    Final Exam | Professional Portfolio

--- portfolio/script.js
+++ portfolio/script.js
@@ -7,3 +7,26 @@ mobileMenuBtn.addEventListener("click", () => {
     mobileMenuBtn.classList.toggle("closed");
     mobileMenu.classList.toggle("closed");
 })
+
+// Scroll to top button
+// Variables
+const scrollTopBtn = document.querySelector("#scrollTopButton");
+const scrollRevealPoint = document.querySelector("header");
+// Function to check scroll position
+function checkVisibility() {
+    // Variables
+    const position = scrollRevealPoint.getBoundingClientRect();
+    const windowHeight = window.innerHeight;
+    // When user scrolls, if header visible: display none, else: display block
+    if (position.top < windowHeight && position.bottom >= 0) {
+        scrollTopBtn.style.display = "none";
+    } else {
+        scrollTopBtn.style.display = "block";
+    }
+}
+// Run function on scroll 
+window.addEventListener('scroll', checkVisibility)
+// When scrollTopBtn clicked, scroll to top of page
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo(0,0);
+})
